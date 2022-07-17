2dfff599f8624e1caf5c5b7fa01e75c11b9af1dd
KRISHNA KUMAR VERMA <56273633+verma29897@users.noreply.github.com>
2022-07-17 12:38:55 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
+   const len = string.length;
+
+    
+    for (let i = 0; i < len / 2; i++) {
+
+        
+        if (string[i] !== string[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 }
 module.exports = palindrome
